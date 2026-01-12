9c170e8ecd37bf0d3c8c45c6b16676dbabe832bd
adaatidev <ada.atienza@iesnervion.es>
2026-01-12 12:39:47 +0100

    Cambios Ada osi

--- src/conflictos/Conflictos.java
+++ src/conflictos/Conflictos.java
@@ -3,7 +3,12 @@ package conflictos;
 public class Conflictos {
 
 	public static void main(String[] args) {
-		
+
+		String nombre = "Ada";
+		int edad = 18;
+		int dni = 123456789;
+
+		System.out.println("Hola, soy " + nombre);
 
 	}
 
