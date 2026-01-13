c8bcdbac74479e23d3910e635fea52903b4b99ba
Usuario <Usuario@DESKTOP-1012429>
2026-01-13 20:03:31 +0100

    Ada

--- src/conflictos/Conflictos.java
+++ src/conflictos/Conflictos.java
@@ -4,13 +4,13 @@ public class Conflictos {
 
 	public static void main(String[] args) {
 
-		String nombre = "Ada";
-		int edad = 19;
-		int dni = 3812931;
+		String nombre = "christian";
+		int edad = 2313;
+		String dni = "24242B";
 
 		System.out.println("Hola, soy " + nombre + " tengo " + edad + " años y mi DNI es " + dni);
 		
-		System.out.println("Miku es god");
+		System.out.println("Miku es bad");
 
 	}
 
