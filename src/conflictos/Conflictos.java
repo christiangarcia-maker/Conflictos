d49fe9c249364436f4f892a021fd8481bc60243c
christian.garcia <christian.garcia@106-03.info2>
2026-01-13 14:28:27 +0100

    dadas

--- src/conflictos/Conflictos.java
+++ src/conflictos/Conflictos.java
@@ -5,7 +5,7 @@ public class Conflictos {
 	public static void main(String[] args) {
 
 		String nombre = "Christian";
-		int edad = 20;
+		int edad = 210;
 		String dni = "2342425B";
 
 		System.out.println("Hola, soy " + nombre + " tengo " + edad + " años y mi DNI es " + dni);
